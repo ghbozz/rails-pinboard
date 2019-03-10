5c06c951d4fb27e8629a31a998f807dedfa06957
romain sanson <romainsanson.gm@gmail.com>
2019-03-10 14:52:57 +0100

    pin model & migration + basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'pins/index'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pins#index'
+  resources :pins, only: %i(index show new create destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
